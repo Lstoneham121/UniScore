c6788d0a4fd672967d0a21a9d2bbcca6ebceb2c1
Lewis Stoneham (s5114822) <s5114822@mac42943.student.bournemouth.ac.uk>
2018-11-22 15:22:58 +0000

    Initial Commit

---
+++ UniScore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniScore
+//
+//  Created by Lewis Stoneham (s5114822) on 22/11/2018.
+//  Copyright © 2018 Lewis Stoneham (s5114822). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
