856a3f86a7f1c4ba27dedf2ee8d093a91ec198ce
Lewis Stoneham (s5114822) <s5114822@mac42943.student.bournemouth.ac.uk>
2018-11-22 15:38:35 +0000

    project starter

--- UniScore/ViewController.swift
+++ UniScore/ViewController.swift
@@ -1,18 +1,13 @@
-//
-//  ViewController.swift
-//  UniScore
-//
-//  Created by Lewis Stoneham (s5114822) on 22/11/2018.
-//  Copyright © 2018 Lewis Stoneham (s5114822). All rights reserved.
-//
+
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
 
